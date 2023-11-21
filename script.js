2bedd73a60b57f248d1b84e7219161b2af29b5fb
raplordrds <habib.hakeem@yahoo.com>
2023-11-21 21:44:38 +0000

    wrote for loop for 9 hours to represent timeblocks

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-// create variables to hold element values with jquery
-$("p#currentDay").text(dayjs())
+// use jQuery syntax to target current day id and tag for more specificity and use .text to append and display to screen
+const currentDate = dayjs()
+$("p#currentDay").text(currentDate.format("dddd-MMMM-YYYY"))
+
+// for loop for 9 hours starting from 9AM
+function timeBlocks(){
+    for(let i=9; i<=17; i++){
+        let hours = dayjs().hour(i).format("hA");  
+        console.log(hours);
+    }
+}
