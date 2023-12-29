26b4379f5ebbebcf242394605529929b361681d1
raplordrds <habib.hakeem@yahoo.com>
2023-12-29 14:16:42 +0000

    successfully created timeblocks using generateTimeBlocks function and added classes to help style with bootstrap

--- script.js
+++ script.js
@@ -14,11 +14,24 @@ function generateTimeBlocks(){
         console.log(hours);
 
         // Create timeblock element and add class ".time-block"
-        let timeBlock = $("<div>").addClass("time-block");
+        let timeBlock = $("<div>").addClass("row time-block");
         // add data attribute representing each hour from 9 hours in [i]
         timeBlock.attr("data-hour", i);
+
+        // create elements for time event and save btn
+        let timeEl = $("<div>").addClass("col-1 time").text(hours);
+        let eventInput = $("<textarea>").attr("id", "event-" + i).addClass("col-10");
+        let saveBtn = $("<button>").addClass("col-1 save-btn").text("save").attr("data-hour", i);
+
+        // Append elements created to timeBlock
+        timeBlock.append(timeEl);
+        timeBlock.append(eventInput);
+        timeBlock.append(saveBtn);
+
+        // Append timeBlock to timeBlocksContainer
+        timeBlocksContainer.append(timeBlock);
     }
 }
 
 
-
+generateTimeBlocks()
