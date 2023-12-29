029be418523994f4462767d78caca9b5cc11a1f6
raplordrds <habib.hakeem@yahoo.com>
2023-12-29 13:30:30 +0000

    created timeblock div, element and added class and data attributes

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 // use jQuery syntax to target current day id and tag for more specificity and use .text to append and display to screen
 const currentDate = dayjs()
-$("p#currentDay").text(currentDate.format("dddd-MMMM-YYYY"))
+$("p#currentDay").text(currentDate.format("dddd-MMMM D-YYYY"));
 
-// for loop for 9 hours starting from 9AM
-function timeBlocks(){
+
+// function to generate timeblocks
+function generateTimeBlocks(){
+    //get Timeblocks div element element by class
+    const timeBlocksContainer = $(".timeBlocks");
+
+    // for loop for 9 hours starting from 9AM
     for(let i=9; i<=17; i++){
         let hours = dayjs().hour(i).format("hA");  
         console.log(hours);
+
+        // Create timeblock element and add class ".time-block"
+        let timeBlock = $("<div>").addClass("time-block");
+        // add data attribute representing each hour from 9 hours in [i]
+        timeBlock.attr("data-hour", i);
     }
 }
+
+
+
